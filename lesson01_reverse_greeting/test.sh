71c2e47d043a53095e0429cd7e07deb6f1cd2f40
Daniil Minin <Minin_D@bankrbk.kz>
2026-06-01 16:38:38 +0500

    lesson01 done

--- lesson01_reverse_greeting/test.sh
+++ lesson01_reverse_greeting/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-expected='!dlroW ,olleH'
+expected='!SUTO ,olleH'
 result=$(go run main.go | sed 's/^ *//;s/ *$//')
 [ "${result}" = "${expected}" ] || (echo -e "invalid output: ${result}" && exit 1)
 
