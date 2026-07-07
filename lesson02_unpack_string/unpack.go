dd34b988162bc857fe71ec9c93f67b1ab6047531
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-07-08 01:51:10 +0300

    refactor: lesson02_unpack_string with no asterisk refactoring

--- lesson02_unpack_string/unpack.go
+++ lesson02_unpack_string/unpack.go
@@ -14,20 +14,25 @@ func Unpack(str string) (string, error) {
 	var result strings.Builder
 
 	for i := 0; i < len(runes); i++ {
+		if unicode.IsLetter(runes[i]) {
 
-		if i+1 < len(runes) && unicode.IsDigit(runes[i]) && (i == 0 || unicode.IsDigit(runes[i+1])) {
+			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+				count := int(runes[i+1] - '0')
+				letter := string(runes[i])
+				newStr := strings.Repeat(letter, count)
 
-			return "", ErrInvalidString
-		} else if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) && unicode.IsLetter(runes[i]) {
-			count := int(runes[i+1] - '0')
-			letter := string(runes[i])
-			newStr := strings.Repeat(letter, count)
+				result.WriteString(newStr)
+			} else {
+				letter := string(runes[i])
 
-			result.WriteString(newStr)
-		} else if unicode.IsLetter(runes[i]) {
-			letter := string(runes[i])
+				result.WriteString(letter)
+			}
 
-			result.WriteString(letter)
+		} else if unicode.IsDigit(runes[i]) {
+
+			if i == 0 || unicode.IsLetter(runes[i-1]) == false && unicode.IsDigit(runes[i-1]) {
+				return "", ErrInvalidString
+			}
 		}
 	}
 
