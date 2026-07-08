2f00687a85421b082fd5facd485a5d16c119770a
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-07-08 04:01:02 +0300

    feat: lesson02_unpack_string with asterisk completed

--- lesson02_unpack_string/unpack.go
+++ lesson02_unpack_string/unpack.go
@@ -14,6 +14,27 @@ func Unpack(str string) (string, error) {
 	var result strings.Builder
 
 	for i := 0; i < len(runes); i++ {
+		if runes[i] == '\\' {
+			i++
+
+			if i >= len(runes) {
+				return "", ErrInvalidString
+			}
+
+			current := runes[i]
+
+			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+				count := int(runes[i+1] - '0')
+
+				result.WriteString(strings.Repeat(string(current), count))
+				i++
+			} else {
+				result.WriteString(string(current))
+			}
+
+			continue
+		}
+
 		if unicode.IsLetter(runes[i]) {
 			current := runes[i]
 
@@ -21,6 +42,7 @@ func Unpack(str string) (string, error) {
 				count := int(runes[i+1] - '0')
 
 				result.WriteString(strings.Repeat(string(current), count))
+				i++
 			} else {
 				result.WriteString(string(current))
 			}
@@ -39,3 +61,37 @@ func Unpack(str string) (string, error) {
 
 	return result.String(), nil
 }
+
+// No asterisk version
+
+//func Unpack(str string) (string, error) {
+//	runes := []rune(str)
+//
+//	var result strings.Builder
+//
+//	for i := 0; i < len(runes); i++ {
+//		if unicode.IsLetter(runes[i]) {
+//			current := runes[i]
+//
+//			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+//				count := int(runes[i+1] - '0')
+//
+//				result.WriteString(strings.Repeat(string(current), count))
+//			} else {
+//				result.WriteString(string(current))
+//			}
+//		} else if unicode.IsDigit(runes[i]) {
+//			if i == 0 {
+//				return "", ErrInvalidString
+//			}
+//
+//			prev := runes[i-1]
+//
+//			if unicode.IsLetter(prev) == false && unicode.IsDigit(prev) {
+//				return "", ErrInvalidString
+//			}
+//		}
+//	}
+//
+//	return result.String(), nil
+//}
