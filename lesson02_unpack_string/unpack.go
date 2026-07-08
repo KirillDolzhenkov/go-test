ea134fa81b6fa8b50eee03bb04154f45335b5173
Kirill <77087981+KirillDolzhenkov@users.noreply.github.com>
2026-07-08 15:16:42 +0300

    feat: lesson02_unpack_string with asterisk completed
    * feat: lesson02_unpack_string with no asterisk completed
    
    * refactor: lesson02_unpack_string with no asterisk refactoring
    
    * refactor: lesson02_unpack_string with no asterisk refactoring
    
    * refactor: lesson02_unpack_string with no asterisk refactoring
    
    * feat: lesson02_unpack_string with asterisk completed

--- lesson02_unpack_string/unpack.go
+++ lesson02_unpack_string/unpack.go
@@ -2,11 +2,96 @@ package lesson02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	runes := []rune(str)
+
+	var result strings.Builder
+
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == '\\' {
+			i++
+
+			if i >= len(runes) {
+				return "", ErrInvalidString
+			}
+
+			current := runes[i]
+
+			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+				count := int(runes[i+1] - '0')
+
+				result.WriteString(strings.Repeat(string(current), count))
+				i++
+			} else {
+				result.WriteString(string(current))
+			}
+
+			continue
+		}
+
+		if unicode.IsLetter(runes[i]) {
+			current := runes[i]
+
+			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+				count := int(runes[i+1] - '0')
+
+				result.WriteString(strings.Repeat(string(current), count))
+				i++
+			} else {
+				result.WriteString(string(current))
+			}
+		} else if unicode.IsDigit(runes[i]) {
+			if i == 0 {
+				return "", ErrInvalidString
+			}
+
+			prev := runes[i-1]
+
+			if unicode.IsLetter(prev) == false && unicode.IsDigit(prev) {
+				return "", ErrInvalidString
+			}
+		}
+	}
+
+	return result.String(), nil
 }
+
+// No asterisk version
+
+//func Unpack(str string) (string, error) {
+//	runes := []rune(str)
+//
+//	var result strings.Builder
+//
+//	for i := 0; i < len(runes); i++ {
+//		if unicode.IsLetter(runes[i]) {
+//			current := runes[i]
+//
+//			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+//				count := int(runes[i+1] - '0')
+//
+//				result.WriteString(strings.Repeat(string(current), count))
+//			} else {
+//				result.WriteString(string(current))
+//			}
+//		} else if unicode.IsDigit(runes[i]) {
+//			if i == 0 {
+//				return "", ErrInvalidString
+//			}
+//
+//			prev := runes[i-1]
+//
+//			if unicode.IsLetter(prev) == false && unicode.IsDigit(prev) {
+//				return "", ErrInvalidString
+//			}
+//		}
+//	}
+//
+//	return result.String(), nil
+//}
